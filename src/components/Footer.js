b70157031e311362f36d0f3e7d126fa03cbb09da
sharma151 <sharmasaurav150503@gmail.com>
2024-01-08 18:26:13 +0530

    text updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed Saurav sharma</h3>
+          <h3>Designed and Developed by Saurav sharma</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} SS</h3>
