4f1aa740ebabd41faf4b306607b2f720b9dc0800
sharma151 <sharmasaurav150503@gmail.com>
2024-01-21 23:13:56 +0530

    resume updated

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Saurav_Resume_2023.pdf";
+import pdf from "../../Assets/saurav_sharma_resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
